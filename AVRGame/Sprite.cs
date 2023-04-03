7feb2af8b1d9442f63b518083565a0d33a062533
Nils van Heezik <nilsvheezik@gmail.com>
2023-04-03 14:35:21 +0200

    probeelsel push 2 stap 2

--- AVRGame/Sprite.cs
+++ AVRGame/Sprite.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace AVRGame
 {
     internal class Sprite
     {
+        protected Texture2D texture;
+        protected Rectangle screen;
+
+        protected Vector2 position;
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        private Direction direction;
+
+        public Direction Direction
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public Rectangle SpriteBox { get { return new Rectangle((int)position.X, 
+            (int)position.Y, texture.Width, texture.Height); } }
+    
+        public Sprite(Texture2D texture, Vector2 position, Direction direction)
+        {
+            this.texture = texture;
+            this.position = position;
+            this.direction = direction;
+        }
+
+        public virtual void Update(GameTime gameTime)
+        {
+
+        }
+
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(texture, position, Color.White);
+        }
     }
+
 }
