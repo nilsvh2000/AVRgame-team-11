eb090a83d44389604801d96627c77302655fb34b
Nils van Heezik <nilsvheezik@gmail.com>
2023-04-03 14:32:51 +0200

    nummer 2

--- AVRGame/Meteor.cs
+++ AVRGame/Meteor.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace AVRGame
 {
-    internal class Meteor
+     class Meteor : Sprite
     {
+        public Meteor(Texture2D texture, Vector2 position, Direction direction) 
+            : base(texture, position, direction)
+        {
+
+        }
     }
 }
